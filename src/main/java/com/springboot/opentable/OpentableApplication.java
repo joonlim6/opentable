e96a09cbf4f7b5fec78c1868cd83c906e0067815
joonlim6 <limjoon629@gmail.com>
2023-10-03 21:57:09 +0900

    init commit

---
+++ src/main/java/com/springboot/opentable/OpentableApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.opentable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpentableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/opentable/OpentableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.opentable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
